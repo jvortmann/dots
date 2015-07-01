bf0c9a50919abe1271f2888b2a09cd16e476128f
Joao Vortmann <jvortmann@gmail.com>
2015-07-01 19:20:16 -0300

    Add initial app installer script for osx

---
+++ osx/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+command -v brew > /dev/null || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+brew tap caskroom/fonts
+
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+cat apps/brew-cask | tr '\n' ' ' | xargs brew cask install
+cat apps/brew | tr '\n' ' ' | xargs brew install
