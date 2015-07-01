8f3b410083bb7a92d4edc61ac66a3a938557e9ae
Joao Vortmann <jvortmann@gmail.com>
2015-07-01 18:48:18 -0300

    Add initial symlink script

---
+++ symlink.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Setting symlinks for default configurations"
+ln -sfhv $(pwd) ~/.dots
+ln -sfhv ~/.dots/ag/agignore ~/.agignore
+ln -sfhv ~/.dots/git/gitignore ~/.gitignore
+ln -sfhv ~/.dots/git/gitconfig ~/.gitconfig
+ln -sfhv ~/.dots/input/inputrc ~/.inputrc
+ln -sfhv ~/.dots/gem/gemrc ~/.gemrc
+ln -sfhv ~/.dots/irb/irbrc ~/.irbrc
