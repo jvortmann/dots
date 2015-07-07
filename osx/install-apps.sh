07b342e45fab9e0d870ad4ae64a00395348a705d
Joao Vortmann <jvortmann@gmail.com>
2015-07-07 18:24:58 -0300

    Improve verbiage and order when installing apps

--- osx/install-apps.sh
+++ osx/install-apps.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-command -v brew > /dev/null || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+command -v brew > /dev/null || (echo; echo ">> Homebrew Versions" && ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)")
 
-brew install caskroom/cask/brew-cask
+echo; echo ">> Homebrew Versions"
 brew tap homebrew/versions
+echo; echo ">> Homebrew Cask"
+brew install caskroom/cask/brew-cask
+echo; echo ">> Homebrew Cask Versions"
 brew tap caskroom/versions
+echo; echo ">> Homebrew Cask Fonts"
 brew tap caskroom/fonts
 
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+echo; echo ">> Installing Apps"
 cat apps/brew-cask | tr '\n' ' ' | xargs brew cask install
+echo; echo ">> Installing Command Line Tools"
 cat apps/brew | tr '\n' ' ' | xargs brew install
