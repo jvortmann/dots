6260d557d61e5f883a8a280efbc78bee1f7c930e
Joao Vortmann <jvortmann@gmail.com>
2015-08-25 18:21:30 -0300

    chore: Use Brewfile for installing apps instead of script

--- osx/install-apps.sh
+++ osx/install-apps.sh
@@ -2,17 +2,7 @@
 
 command -v brew > /dev/null || (echo; echo ">> Homebrew Versions" && ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)")
 
-echo; echo ">> Homebrew Versions"
-brew tap homebrew/versions
-echo; echo ">> Homebrew Cask"
-brew install caskroom/cask/brew-cask
-echo; echo ">> Homebrew Cask Versions"
-brew tap caskroom/versions
-echo; echo ">> Homebrew Cask Fonts"
-brew tap caskroom/fonts
-
+brew tap Homebrew/bundle
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
-echo; echo ">> Installing Apps"
-cat apps/brew-cask | tr '\n' ' ' | xargs brew cask install
-echo; echo ">> Installing Command Line Tools"
-cat apps/brew | tr '\n' ' ' | xargs brew install
+
+brew bundle
