8474a6631ded484ca8cdfa8681214ed6a9496a6b
Joao Vortmann <jvortmann@gmail.com>
2015-09-15 17:17:50 -0300

    feature: Remove guard message on status bar in favor of another notifier

--- guard/guard.rb
+++ guard/guard.rb
@@ -1,1 +1,1 @@
-notification :tmux, color_location: "status-right-fg", default: "yellow", success: "green", display_message: true, timeout: 2
+notification :tmux, color_location: "status-right-fg", default: "yellow", success: "green", display_message: false, timeout: 2
