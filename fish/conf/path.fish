55aa0b6d2abc206c73c79c0f8474f4a06445b4bd
Joao Vortmann <jvortmann@gmail.com>
2016-01-11 11:51:16 -0200

    feature: Remove unnecessary homebrew path setup

--- fish/conf/path.fish
+++ fish/conf/path.fish
@@ -1,9 +1,6 @@
-set default_path /usr/bin /usr/sbin /bin /sbin
-set homebrew /usr/local/bin
-
 # add basher to path when present
 if test -d ~/.basher
   set basher ~/.basher/bin
 end
 
-set -gx PATH $basher $homebrew $default_path
+set -gx PATH $basher $PATH
