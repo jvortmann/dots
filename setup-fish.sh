5573a93a1709a64260cd21dc65a721d87ed48965
Joao Vortmann <jvortmann@gmail.com>
2016-01-19 10:08:29 -0200

    feature: Remove the need for user on the setup configuration for fish

--- setup-fish.sh
+++ setup-fish.sh
@@ -2,7 +2,7 @@
 
 echo "Adding Fish to /etc/shells and set it as default shell (needs sudo)"
 sudo grep "fish" /etc/shells || echo $(which fish) | sudo tee -a /etc/shells
-chsh -s $(which fish) jv
+chsh -s $(which fish)
 
 echo "Adding Fish configs to ~/.config/fish"
 mkdir -pv .config/fish/
