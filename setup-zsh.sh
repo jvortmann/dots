c71fec958132c96dfe5c7d0c2dc0dc20b5fe24f2
Marcelo Boeira <me@marceloboeira.com>
2016-08-10 17:46:56 +0200

    feature: Use the current user to set zsh instead

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -2,7 +2,7 @@
 
 echo "Adding Zsh to /etc/shells and set it as default shell (needs sudo)"
 sudo grep "$(which zsh)" /etc/shells || echo $(which zsh) | sudo tee -a /etc/shells
-chsh -s $(which zsh) jv
+chsh -s $(which zsh) $(whoami)
 
 echo "Symlink Zsh configs to ~/zshrc"
 ln -sfhv ~/.dots/zsh/zshrc ~/.zshrc
