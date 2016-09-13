23fac481ae88a819f6bf9b39a2bdcf154d06e10c
Joao Vortmann <jvortmann@gmail.com>
2016-09-13 13:06:25 -0300

    refactor: Use shorter way to echo blank line

--- symlink.sh
+++ symlink.sh
@@ -1,33 +1,26 @@
 #!/bin/bash
 
-echo
-echo ">> Creating ~/.config folder"
+echo; echo ">> Creating ~/.config folder"
 
 mkdir -p ~/.config/
 
-echo
-echo ">> Setting symlinks for dots folder"
+echo; echo ">> Setting symlinks for dots folder"
 ln -sfhv $(pwd) ~/.dots
 
-echo
-echo ">> Setting symlinks for default configurations"
+echo; echo ">> Setting symlinks for default configurations"
 ln -sfhv ~/.dots/ag/agignore ~/.agignore
 ln -sfhv ~/.dots/input/inputrc ~/.inputrc
 ln -sfhv ~/.dots/ctags/ctags ~/.ctags
 
-echo
-echo ">> Setting symlinks for ruby configurations"
+echo; echo ">> Setting symlinks for ruby configurations"
 ln -sfhv ~/.dots/gem/gemrc ~/.gemrc
 ln -sfhv ~/.dots/irb/irbrc ~/.irbrc
 ln -sfhv ~/.dots/guard/guard.rb ~/.guard.rb
 
-echo
-echo ">> Setting symlinks for tmux configurations"
+echo; echo ">> Setting symlinks for tmux configurations"
 ln -sfhv ~/.dots/tmux/tmux.conf ~/.tmux.conf
 
-echo
-echo ">> Setting symlinks for git"
+echo; echo ">> Setting symlinks for git"
 mkdir -p ~/.config/git
 ln -sfhv ~/.dots/git/ignore ~/.config/git/ignore
 ln -sfhv ~/.dots/git/config ~/.config/git/config
-
