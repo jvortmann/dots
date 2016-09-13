423f42fd6caa1106f8155b1e3c981c07eb8aacc4
Joao Vortmann <jvortmann@gmail.com>
2016-09-13 12:38:03 -0300

    feature: Use new path for git config files

--- symlink.sh
+++ symlink.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-echo "Creating ~/.config folder"
+echo ">> Creating ~/.config folder"
 mkdir -p ~/.config/
 
-echo "Setting symlinks for default configurations"
+echo ">> Setting symlinks for default configurations"
 ln -sfhv $(pwd) ~/.dots
 ln -sfhv ~/.dots/ag/agignore ~/.agignore
-ln -sfhv ~/.dots/git/gitignore ~/.gitignore
-ln -sfhv ~/.dots/git/gitconfig ~/.gitconfig
+
+echo ">> Setting symlinks for git"
+mkdir -p ~/.config/git
+ln -sfhv ~/.dots/git/ignore ~/.config/git/ignore
+ln -sfhv ~/.dots/git/config ~/.config/git/config
+
 ln -sfhv ~/.dots/input/inputrc ~/.inputrc
 ln -sfhv ~/.dots/gem/gemrc ~/.gemrc
 ln -sfhv ~/.dots/irb/irbrc ~/.irbrc
