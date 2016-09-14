7f389d20104770c97f94a47fc5f29e04faff5556
João Vortmann <jvortmann@gmail.com>
2016-09-14 15:12:45 -0300

    feature: Remove unnecessary setting

--- osx/install-apps.sh
+++ osx/install-apps.sh
@@ -3,6 +3,5 @@
 command -v brew > /dev/null || (echo; echo ">> Homebrew Versions" && ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)")
 
 brew tap Homebrew/bundle
-export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
 brew bundle --verbose
