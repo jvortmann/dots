599528e7cb5b65c95818c36824bf8ff234ebfc05
João Vortmann <jvortmann@gmail.com>
2016-09-14 15:13:28 -0300

    chore: Improve verbiage

--- osx/install-apps.sh
+++ osx/install-apps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-command -v brew > /dev/null || (echo; echo ">> Homebrew Versions" && ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)")
+command -v brew > /dev/null || (echo; echo ">> Homebrew" && ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)")
 
 brew tap Homebrew/bundle
 
