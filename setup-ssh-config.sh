c056b43497b0abed6995930eeb9301a309bb4833
João Vortmann <jvortmann@gmail.com>
2016-10-26 16:54:56 -0200

    feature: Add ssh config template generation using computer name

---
+++ setup-ssh-config.sh
@@ -0,0 +1,34 @@
+if [ $# -ne 1 ]
+then
+  echo 'Usage: ./setup-ssh-config.sh "Computer Name"'
+  exit 1
+fi
+
+computer_name="$1"
+
+echo; echo ">> Setting ssh config file"
+cat > "$HOME/.ssh/config" <<EOF
+# vim: ft=sshconfig
+
+Host heroku
+  HostName heroku.com
+  User git
+  IdentityFile ~/.ssh/$computer_name
+  IdentitiesOnly yes
+
+Host github
+  HostName github.com
+  User git
+  IdentityFile ~/.ssh/$computer_name
+  IdentitiesOnly yes
+
+Host bitbucket
+  HostName bitbucket.org
+  User git
+  IdentityFile ~/.ssh/$computer_name
+
+Host globo
+  HostName gitlab.globoi.com
+  User gitlab
+  IdentityFile ~/.ssh/$computer_name
+EOF
