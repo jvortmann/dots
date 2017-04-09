a434a47eff00eb24c535881846b3215383761b25
João Vortmann <jvortmann@gmail.com>
2017-04-09 16:02:04 -0300

    refactor: Use zplug to install zsh related extras

--- zsh/conf/plugins.zsh
+++ zsh/conf/plugins.zsh
@@ -1,3 +1,8 @@
 zplug "$ZSH_HOME/conf/plugins", from:local
 
+zplug "zsh-users/zsh-autosuggestions"
+zplug "zsh-users/zsh-completions"
+zplug "zsh-users/zsh-history-substring-search"
+zplug "zsh-users/zsh-syntax-highlighting"
+
 zplug load
