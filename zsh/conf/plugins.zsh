e5b5225756cfe27b0c2179dc7f7e71b684c87f3b
João Vortmann <jvortmann@gmail.com>
2017-04-10 16:16:28 -0300

    feature: Remove zplug

--- zsh/conf/plugins.zsh
+++ zsh/conf/plugins.zsh
@@ -1,10 +1,2 @@
-zplug "$ZSH_HOME/conf/plugins", from:local
-
-zplug "zsh-users/zsh-autosuggestions"
-zplug "zsh-users/zsh-completions"
-zplug "zsh-users/zsh-history-substring-search"
-zplug "zsh-users/zsh-syntax-highlighting"
-
-zplug 'zplug/zplug', hook-build:'zplug --self-manage'
-
-zplug load
+# vim: ft=zsh
+for file ($ZSH_HOME/conf/plugins/*.zsh) source $file
