84eb79f22ac7017715beeeede3d6967ae878e29d
João Vortmann <jvortmann@gmail.com>
2017-04-10 18:48:08 -0300

    feature: Cache generation of yarn path for a day

--- zsh/conf/plugins/yarn.zsh
+++ zsh/conf/plugins/yarn.zsh
@@ -1,3 +1,10 @@
 # vim: ft=zsh
 
-#export PATH="$(yarn global bin):$PATH"
+local cache_path="$ZSH_HOME/conf/settings/.yarn_path_cache"
+local cache_is_old=$(find "$cache_path" -mtime -1d 2> /dev/null)
+
+if [ ! -r "$cache_is_old" ]; then
+  $(yarn global bin > $cache_path)
+fi
+
+export PATH="$(cat $cache_path):$PATH"
