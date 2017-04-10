39ecd5bb09c6c7ecaff2192bd3934386ebfb7704
João Vortmann <jvortmann@gmail.com>
2017-04-10 18:52:44 -0300

    feature: Change the path to the yarn path cache file

--- zsh/conf/plugins/yarn.zsh
+++ zsh/conf/plugins/yarn.zsh
@@ -1,6 +1,6 @@
 # vim: ft=zsh
 
-local cache_path="$ZSH_HOME/conf/settings/.yarn_path_cache"
+local cache_path="$ZSH_HOME/conf/plugins/.yarn_path_cache"
 local cache_is_old=$(find "$cache_path" -mtime -1d 2> /dev/null)
 
 if [ ! -r "$cache_is_old" ]; then
