a3213da995dbea6b527ca417742f4c3e10d88c3d
João Vortmann <jvortmann@gmail.com>
2017-04-12 16:56:59 -0300

    feature: Remove yarn plugin

--- zsh/conf/plugins/yarn.zsh
+++
@@ -1,7 +0,0 @@
-# vim: ft=zsh
-
-export YARN_PATH_CACHEFILE="$ZSH_HOME/conf/plugins/.yarn_path_cache"
-
-cache "$YARN_PATH_CACHEFILE" "1d" "yarn global bin"
-
-export PATH="$(cat $YARN_PATH_CACHEFILE):$PATH"
