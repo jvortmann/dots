f43e0588cddee8528ab6abdfffaa4d1cab673b35
João Vortmann <jvortmann@gmail.com>
2017-07-21 11:59:56 -0300

    feature: Add pyhton version info to prompt

---
+++ zsh/conf/plugins/pyenv.zsh
@@ -0,0 +1,22 @@
+# vim: ft=zsh
+
+if [ -d "$HOME/.pyenv" ]; then
+  export PYENV_ROOT="$HOME/.pyenv"
+  export PATH="$PYENV_ROOT/bin:$PATH"
+
+  export PYENV_INIT_CACHEFILE="$ZSH_HOME/conf/plugins/.pyenv_init_cache"
+
+  cache "$PYENV_INIT_CACHEFILE" "1d" "pyenv init -" "export PATH=\"$HOME/.pyenv/shims:$PATH\""
+fi
+
+# prompt function
+python_version_info() {
+  local name=$(pyenv version-name 2> /dev/null)
+  local origin=$(pyenv version-origin 2> /dev/null)
+
+  if [[ $origin =~ "python-version" && $name != "system" ]]; then
+    echo "[python:$name]"
+  else
+    echo ""
+  fi
+}
--- zsh/themes/jvortmann.zsh
+++ zsh/themes/jvortmann.zsh
@@ -15,11 +15,17 @@ then
   node_version_info() { }
 fi
 
+if ! declare -f python_version_info > /dev/null
+then
+  python_version_info() { }
+fi
+
 update_prompt() {
   _user_and_host="${BLUE}%n@%m${RESET}"
   _cwd="${GREEN}%1~${RESET}"
   _ruby_info="${RED}$(ruby_version_info)${RESET}"
   _node_info="${GREEN}$(node_version_info)${RESET}"
+  _python_info="${BLUE}$(python_version_info)${RESET}"
 
   # display exitcode on the right when >0
   _return_code=" %(0?..${RED}%? ↵${RESET})"
@@ -27,9 +33,9 @@ update_prompt() {
   PROMPT='${_user_and_host} ${_cwd} $(git_prompt_string) ∴ ${RESET}'
 
   if type gdate > /dev/null; then
-    RPROMPT='${_ruby_info}${_node_info}(${LAST_COMMAND_TIME}ms)${_return_code}${RESET}'
+    RPROMPT='${_ruby_info}${_python_info}${_node_info}(${LAST_COMMAND_TIME}ms)${_return_code}${RESET}'
   else
-    RPROMPT='${_ruby_info}${_node_info}${return_code}${RESET}'
+    RPROMPT='${_ruby_info}${_python_info}${_node_info}${return_code}${RESET}'
   fi
 }
 
