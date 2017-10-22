62059abb38b2f898c77cd24e46eb2d1313aa5c03
João Vortmann <jvortmann@gmail.com>
2017-10-22 16:31:43 -0200

    fix: Change path to zsh-history-substring-search init script (after update)

--- zsh/conf/plugins/zsh-history-substring-search.zsh
+++ zsh/conf/plugins/zsh-history-substring-search.zsh
@@ -1,5 +1,5 @@
 # vim: ft=zsh
 
-if [ -r /usr/local/opt/zsh-history-substring-search/zsh-history-substring-search.zsh ]; then
-  source /usr/local/opt/zsh-history-substring-search/zsh-history-substring-search.zsh
+if [ -r /usr/local/share/zsh-history-substring-search/zsh-history-substring-search.zsh ]; then
+  source /usr/local/share/zsh-history-substring-search/zsh-history-substring-search.zsh
 fi
