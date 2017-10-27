525c35f5032500ad9c6a5a4c8ef8f07a5b65c5b4
João Vortmann <jvortmann@gmail.com>
2017-10-27 12:50:50 -0200

    fix: Use -n instead -h on symlinks for compatibility

--- symlink.sh
+++ symlink.sh
@@ -5,23 +5,23 @@ echo; echo ">> Creating ~/.config folder"
 mkdir -p ~/.config/
 
 echo; echo ">> Setting symlinks for dots folder"
-ln -sfhv "$(pwd)" ~/.dots
+ln -sfnv "$(pwd)" ~/.dots
 
 echo; echo ">> Setting symlinks for default configurations"
-ln -sfhv ~/.dots/ag/agignore ~/.agignore
-ln -sfhv ~/.dots/input/inputrc ~/.inputrc
-ln -sfhv ~/.dots/ctags/ctags ~/.ctags
+ln -sfnv ~/.dots/ag/agignore ~/.agignore
+ln -sfnv ~/.dots/input/inputrc ~/.inputrc
+ln -sfnv ~/.dots/ctags/ctags ~/.ctags
 
 echo; echo ">> Setting symlinks for ruby configurations"
-ln -sfhv ~/.dots/gem/gemrc ~/.gemrc
-ln -sfhv ~/.dots/irb/irbrc ~/.irbrc
-ln -sfhv ~/.dots/guard/guard.rb ~/.guard.rb
+ln -sfnv ~/.dots/gem/gemrc ~/.gemrc
+ln -sfnv ~/.dots/irb/irbrc ~/.irbrc
+ln -sfnv ~/.dots/guard/guard.rb ~/.guard.rb
 
 echo; echo ">> Setting symlinks for eslint configurations"
-ln -sfhv ~/.dots/eslint/eslintrc ~/.eslintrc
+ln -sfnv ~/.dots/eslint/eslintrc ~/.eslintrc
 
 echo; echo ">> Setting symlinks for tmux configurations"
-ln -sfhv ~/.dots/tmux/tmux.conf ~/.tmux.conf
+ln -sfnv ~/.dots/tmux/tmux.conf ~/.tmux.conf
 
 echo; echo ">> Setting symlinks for git"
-ln -sfhv ~/.dots/git ~/.config/git
+ln -sfnv ~/.dots/git ~/.config/git
