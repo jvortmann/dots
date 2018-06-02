c6cc24558b0e5152b0c14ab2ae89786ae8a05918
João Vortmann <jvortmann@gmail.com>
2018-06-02 17:32:09 -0300

    feature: Add elixir versino to prompt

--- zsh/themes/jvortmann.zsh
+++ zsh/themes/jvortmann.zsh
@@ -20,12 +20,18 @@ then
   python_version_info() { }
 fi
 
+if ! declare -f elixir_version_info > /dev/null
+then
+  elixir_version_info() { }
+fi
+
 update_prompt() {
   _user_and_host="${BLUE}%n@%m${RESET}"
   _cwd="${GREEN}%1~${RESET}"
   _ruby_info="${RED}$(ruby_version_info)${RESET}"
   _node_info="${GREEN}$(node_version_info)${RESET}"
   _python_info="${BLUE}$(python_version_info)${RESET}"
+  _elixir_info="${PURPLE}$(elixir_version_info)${RESET}"
 
   # display exitcode on the right when >0
   _return_code=" %(0?..${RED}%? ↵${RESET})"
@@ -33,9 +39,9 @@ update_prompt() {
   PROMPT='${_user_and_host} ${_cwd} $(git_prompt_string) ∴ ${RESET}'
 
   if type gdate > /dev/null; then
-    RPROMPT='${_ruby_info}${_python_info}${_node_info}(${LAST_COMMAND_TIME}ms)${_return_code}${RESET}'
+    RPROMPT='${_ruby_info}${_elixir_info}${_python_info}${_node_info}(${LAST_COMMAND_TIME}ms)${_return_code}${RESET}'
   else
-    RPROMPT='${_ruby_info}${_python_info}${_node_info}${return_code}${RESET}'
+    RPROMPT='${_ruby_info}${_elixir_info}${_python_info}${_node_info}${return_code}${RESET}'
   fi
 }
 
