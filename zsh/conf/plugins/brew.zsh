6b8a2be6e8ff4812e05d0c704b846fa9d11b724e
João Vortmann <jvortmann@gmail.com>
2019-05-03 19:56:17 -0300

    feature: Change brew update to be only manually

--- zsh/conf/plugins/brew.zsh
+++ zsh/conf/plugins/brew.zsh
@@ -3,5 +3,4 @@
 fpath=(/usr/local/share/zsh/site-functions /usr/local/share/zsh-completions $fpath)
 
 export BREW_PREFIX=$(brew --prefix)
-
-export HOMEBREW_AUTO_UPDATE_SECS=432000
+export HOMEBREW_NO_AUTO_UPDATE=1
