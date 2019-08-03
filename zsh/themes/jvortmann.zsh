1c32eac6f938421ee33a2cbb7e7bf14a0dee1ff9
João Vortmann <jvortmann@gmail.com>
2019-08-03 20:17:45 -0300

    feature: Remove node version managing for the moment

--- zsh/themes/jvortmann.zsh
+++ zsh/themes/jvortmann.zsh
@@ -10,11 +10,6 @@ then
   ruby_version_info() { }
 fi
 
-if ! declare -f node_version_info > /dev/null
-then
-  node_version_info() { }
-fi
-
 if ! declare -f python_version_info > /dev/null
 then
   python_version_info() { }
@@ -24,7 +19,6 @@ update_prompt() {
   _user_and_host="${BLUE}%n@%m${RESET}"
   _cwd="${GREEN}%1~${RESET}"
   _ruby_info="${RED}$(ruby_version_info)${RESET}"
-  _node_info="${GREEN}$(node_version_info)${RESET}"
   _python_info="${BLUE}$(python_version_info)${RESET}"
   _elixir_info="${MAGENTA}$(version_info elixir)${RESET}"
 
@@ -34,9 +28,9 @@ update_prompt() {
   PROMPT='${_user_and_host} ${_cwd} $(git_prompt_string) ∴ ${RESET}'
 
   if type gdate > /dev/null; then
-    RPROMPT='${_ruby_info}${_elixir_info}${_python_info}${_node_info}(${LAST_COMMAND_TIME}ms)${_return_code}${RESET}'
+    RPROMPT='${_ruby_info}${_elixir_info}${_python_info}(${LAST_COMMAND_TIME}ms)${_return_code}${RESET}'
   else
-    RPROMPT='${_ruby_info}${_elixir_info}${_python_info}${_node_info}${return_code}${RESET}'
+    RPROMPT='${_ruby_info}${_elixir_info}${_python_info}${return_code}${RESET}'
   fi
 }
 
