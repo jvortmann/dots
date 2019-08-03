a1587889f709a93242f12255baa1c5bd20e28c78
João Vortmann <jvortmann@gmail.com>
2019-08-03 20:45:49 -0300

    feature: Add rust version to the prompt

--- zsh/themes/jvortmann.zsh
+++ zsh/themes/jvortmann.zsh
@@ -16,6 +16,7 @@ update_prompt() {
   _ruby_info="${RED}$(ruby_version_info)${RESET}"
   _python_info="${BLUE}$(version_info python)${RESET}"
   _elixir_info="${MAGENTA}$(version_info elixir)${RESET}"
+  _rust_info="${ORANGE}$(version_info rust)${RESET}"
 
   # display exitcode on the right when >0
   _return_code=" %(0?..${RED}%? ↵${RESET})"
@@ -23,9 +24,9 @@ update_prompt() {
   PROMPT='${_user_and_host} ${_cwd} $(git_prompt_string) ∴ ${RESET}'
 
   if type gdate > /dev/null; then
-    RPROMPT='${_ruby_info}${_elixir_info}${_python_info}(${LAST_COMMAND_TIME}ms)${_return_code}${RESET}'
+    RPROMPT='${_ruby_info}${_rust_info}${_elixir_info}${_python_info}(${LAST_COMMAND_TIME}ms)${_return_code}${RESET}'
   else
-    RPROMPT='${_ruby_info}${_elixir_info}${_python_info}${return_code}${RESET}'
+    RPROMPT='${_ruby_info}${_rust_info}${_python_info}${return_code}${RESET}'
   fi
 }
 
