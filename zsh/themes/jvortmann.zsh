36ae2de7314d4426e4be97fd93709a6bcff690d7
João Vortmann <jvortmann@gmail.com>
2019-08-03 21:05:42 -0300

    feature: Add golang version tag

--- zsh/themes/jvortmann.zsh
+++ zsh/themes/jvortmann.zsh
@@ -12,6 +12,7 @@ update_prompt() {
   _python_info="${BLUE}$(version_info python)${RESET}"
   _elixir_info="${MAGENTA}$(version_info elixir)${RESET}"
   _rust_info="${ORANGE}$(version_info rust)${RESET}"
+  _go_info="${CYAN}$(version_info golang)${RESET}"
 
   # display exitcode on the right when >0
   _return_code=" %(0?..${RED}%? ↵${RESET})"
@@ -19,9 +20,9 @@ update_prompt() {
   PROMPT='${_user_and_host} ${_cwd} $(git_prompt_string) ∴ ${RESET}'
 
   if type gdate > /dev/null; then
-    RPROMPT='${_ruby_info}${_rust_info}${_elixir_info}${_python_info}(${LAST_COMMAND_TIME}ms)${_return_code}${RESET}'
+    RPROMPT='${_ruby_info}${_rust_info}${_elixir_info}${_python_info}${_go_info}(${LAST_COMMAND_TIME}ms)${_return_code}${RESET}'
   else
-    RPROMPT='${_ruby_info}${_rust_info}${_python_info}${_return_code}${RESET}'
+    RPROMPT='${_ruby_info}${_rust_info}${_python_info}${_go_info}${_return_code}${RESET}'
   fi
 }
 
