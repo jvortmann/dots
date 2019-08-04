491959381bb6b536f1303b4f1538f28d65c0300e
João Vortmann <jvortmann@gmail.com>
2019-08-04 18:15:01 -0300

    feature: Change prompt to use composition of sections

--- zsh/themes/jvortmann.zsh
+++ zsh/themes/jvortmann.zsh
@@ -11,9 +11,11 @@ update_prompt() {
   _cwd="${GREEN}%1~${RESET}"
   
   # display exitcode on the right when >0
-  _return_code=" %(0?..${RED}%? ↵${RESET})"
+  _return_code="%(0?..${RED}%?↵${RESET})"
 
   PROMPT='${_user_and_host} ${_cwd} $(git_prompt_string) ∴ ${RESET}'
+
+  _rprompt=""
 
   if declare -f version_info > /dev/null; then
     _ruby_info="${RED}$(version_info ruby)${RESET}"
@@ -22,18 +24,16 @@ update_prompt() {
     _rust_info="${ORANGE}$(version_info rust)${RESET}"
     _go_info="${CYAN}$(version_info golang)${RESET}"
 
-    if type gdate > /dev/null; then
-      RPROMPT='${_ruby_info}${_rust_info}${_elixir_info}${_python_info}${_go_info}(${LAST_COMMAND_TIME}ms)${_return_code}${RESET}'
-    else
-      RPROMPT='${_ruby_info}${_rust_info}${_elixir_info}${_python_info}${_go_info}${_return_code}${RESET}'
-    fi
-  else
-    if type gdate > /dev/null; then
-      RPROMPT='(${LAST_COMMAND_TIME}ms)${_return_code}${RESET}'
-    else
-      RPROMPT='${_return_code}${RESET}'
-    fi
+    _language_tags="${_ruby_info}${_rust_info}${_elixir_info}${_python_info}${_go_info}"
+
+    _rprompt="$_rprompt$_language_tags"
   fi
+
+  if type gdate > /dev/null; then
+    _rprompt="$_rprompt(\${LAST_COMMAND_TIME}ms)"
+  fi
+
+  RPROMPT="$_rprompt$_return_code"
 }
 
 update_prompt
