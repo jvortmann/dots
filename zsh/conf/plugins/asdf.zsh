733d8dc6230c66a61539bb076a4326d0f2fdfee3
João Vortmann <jvortmann@gmail.com>
2019-08-04 19:28:18 -0300

    feature: Change version checking to use grep

--- zsh/conf/plugins/asdf.zsh
+++ zsh/conf/plugins/asdf.zsh
@@ -4,13 +4,20 @@ source "/usr/local/opt/asdf/asdf.sh"
 
 version_info() {
   local language_name=$1
-  local info="$(asdf current $language_name 2> /dev/null)"
+  local version_from_tool_versions=$(grep "$language_name" "$PWD/.tool-versions" &> /dev/null | cut -d' ' -f2)
+  local version_from_language_version=$(ls -a "$PWD" | grep ".$language_name" | xargs cat)
 
-  if [[ $info =~ $PWD ]]; then
-    local version="$(echo $info | cut -d' ' -f1)"
-
-    echo "[$language_name:$version]"
+  if [[ -n $version_from_tool_versions ]]; then
+    version_info_tag $language_name $version_from_tool_versions
   else
-    echo ""
+    if [[ -n $version_from_language_version ]]; then
+      version_info_tag $language_name $version_from_language_version
+    else
+      echo ""
+    fi
   fi
+}
+
+version_info_tag() {
+  echo "[$1:$2]"
 }
