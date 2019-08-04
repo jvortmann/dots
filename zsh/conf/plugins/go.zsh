3360a3474b9b6a579e5fb393b45341e7656d276a
João Vortmann <jvortmann@gmail.com>
2019-08-03 21:03:15 -0300

    feature: Remove unnecessary go configuration

--- zsh/conf/plugins/go.zsh
+++
@@ -1,7 +0,0 @@
-# vim: ft=zsh
-
-# this is required because go completions are not
-# configured to be autoloaded
-if [ -e /usr/local/share/zsh/site-functions/_go ]; then
-  source /usr/local/share/zsh/site-functions/_go
-fi
