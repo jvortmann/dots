15ace977e7a9f00298f968844c999e0ad83c20d4
João Vortmann <jvortmann@gmail.com>
2019-08-03 21:09:20 -0300

    feature: Only show language versions if function is available

--- zsh/themes/jvortmann.zsh
+++ zsh/themes/jvortmann.zsh
@@ -8,21 +8,25 @@ fi
 update_prompt() {
   _user_and_host="${BLUE}%n@%m${RESET}"
   _cwd="${GREEN}%1~${RESET}"
-  _ruby_info="${RED}$(version_info ruby)${RESET}"
-  _python_info="${BLUE}$(version_info python)${RESET}"
-  _elixir_info="${MAGENTA}$(version_info elixir)${RESET}"
-  _rust_info="${ORANGE}$(version_info rust)${RESET}"
-  _go_info="${CYAN}$(version_info golang)${RESET}"
-
   # display exitcode on the right when >0
   _return_code=" %(0?..${RED}%? ↵${RESET})"
 
   PROMPT='${_user_and_host} ${_cwd} $(git_prompt_string) ∴ ${RESET}'
 
-  if type gdate > /dev/null; then
-    RPROMPT='${_ruby_info}${_rust_info}${_elixir_info}${_python_info}${_go_info}(${LAST_COMMAND_TIME}ms)${_return_code}${RESET}'
+  if declare -f version_info > /dev/null; then
+    _ruby_info="${RED}$(version_info ruby)${RESET}"
+    _python_info="${BLUE}$(version_info python)${RESET}"
+    _elixir_info="${MAGENTA}$(version_info elixir)${RESET}"
+    _rust_info="${ORANGE}$(version_info rust)${RESET}"
+    _go_info="${CYAN}$(version_info golang)${RESET}"
+
+    if type gdate > /dev/null; then
+      RPROMPT='${_ruby_info}${_rust_info}${_elixir_info}${_python_info}${_go_info}(${LAST_COMMAND_TIME}ms)${_return_code}${RESET}'
+    else
+      RPROMPT='${_ruby_info}${_rust_info}${_python_info}${_go_info}${_return_code}${RESET}'
+    fi
   else
-    RPROMPT='${_ruby_info}${_rust_info}${_python_info}${_go_info}${_return_code}${RESET}'
+    RPROMPT='${_return_code}${RESET}'
   fi
 }
 
