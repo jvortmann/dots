498eeacaa9635a0bde66c1ebf465aeb4ec56295a
João Vortmann <jvortmann@gmail.com>
2019-10-09 12:20:56 -0300

    feature: Move definition of fpath to completion settings

--- zsh/conf/plugins/brew.zsh
+++ zsh/conf/plugins/brew.zsh
@@ -1,7 +1,5 @@
 # vim: ft=zsh
 
-fpath=(/usr/local/share/zsh/site-functions /usr/local/share/zsh-completions $fpath)
-
 export BREW_PREFIX=$(brew --prefix)
 export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_NO_INSTALL_CLEANUP=1
