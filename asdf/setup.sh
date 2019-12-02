ddd2dade0b121a4c2abe259cc70d2f56e5c1fc52
João Vortmann <jvortmann@gmail.com>
2019-12-02 12:03:42 -0300

    feature: Add mode languages to asdf by default

--- asdf/setup.sh
+++ asdf/setup.sh
@@ -3,9 +3,14 @@ echo ">> [ASDF]  Setting symlink for 'asdfrc'"
 ln -sfnv ~/.dots/asdf/asdfrc ~/.asdfrc
 
 echo; echo ">> [ASDF] Adding plugins"
+asdf plugin-add clojure
+asdf plugin-add elixir
+asdf plugin-add erlang
+asdf plugin-add golang
+asdf plugin-add java
+asdf plugin-add lua
+asdf plugin-add nodejs
+asdf plugin-add python
 asdf plugin-add ruby
 asdf plugin-add rust
-asdf plugin-add elixir
-asdf plugin-add python
-asdf plugin-add golang
-asdf plugin-add erlang
+asdf plugin-add yarn
