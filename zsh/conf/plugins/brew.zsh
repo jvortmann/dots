29f9fa6a5765b400ce7929f823bcfa8be3fdfa02
João Vortmann <jvortmann@gmail.com>
2020-03-23 18:10:27 -0300

    feature: Inline 'brew --prefix' to speed up startup time

--- zsh/conf/plugins/brew.zsh
+++ zsh/conf/plugins/brew.zsh
@@ -1,5 +1,6 @@
 # vim: ft=zsh
 
-export BREW_PREFIX=$(brew --prefix)
+#export BREW_PREFIX=$(brew --prefix)
+export BREW_PREFIX=/usr/local
 export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_NO_INSTALL_CLEANUP=1
