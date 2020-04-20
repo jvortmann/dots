c5d3b293e64f1fa957cb32651d1bc879b2f7bf9f
João Vortmann <jvortmann@gmail.com>
2020-04-19 21:29:32 -0300

    feature: Add a function to clean episode files up

---
+++ zsh/conf/plugins/files.zsh
@@ -0,0 +1,5 @@
+# vim: ft=zsh
+
+cleanup_episode_filename() {
+  mv -v "$1" "$(echo "$1" | sed -E "s/(.*)\.([sS][[:digit:]]{2}[eE][[:digit:]]{2})(.*)?(\.1080|720).*(\..{3})$/\1 - \2 - \3\5/" | tr "." " " | tr -s " " | sed -E "s/(.*) (.{3})$/\1.\2/")"
+}
