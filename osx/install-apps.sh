9560b22bdb0deccf62fdc96fef0463a228e3b636
Joao Vortmann <jvortmann@pm.me>
2020-05-29 18:37:47 -0300

    refactor: Use lowercase tap repo and avoid creation of lock file

--- osx/install-apps.sh
+++ osx/install-apps.sh
@@ -2,10 +2,10 @@
 
 command -v brew > /dev/null || (echo; echo ">> Homebrew" && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)")
 
-brew tap Homebrew/bundle
+brew tap homebrew/bundle
 
 echo; echo ">> Installing Apps"
-brew bundle --verbose
+brew bundle --verbose --no-lock
 
 echo; echo ">> Installing Basher"
 git clone https://github.com/basherpm/basher.git ~/.basher
