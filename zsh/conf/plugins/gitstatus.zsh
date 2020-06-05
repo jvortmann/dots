13f4ae07c2c32bc91783cbab9db87f9ba1774c3e
Joao Vortmann <jvortmann@pm.me>
2020-06-05 20:18:46 -0300

    feature: Show staged deletion in git status as well

--- zsh/conf/plugins/gitstatus.zsh
+++ zsh/conf/plugins/gitstatus.zsh
@@ -62,7 +62,7 @@ git_status() {
     GIT_STATE=$GIT_STATE$_GIT_PROMPT_MODIFIED
   fi
 
-  if echo $STATUS | grep '^[AMRC].' --quiet; then
+  if echo $STATUS | grep '^[AMRCD].' --quiet; then
     GIT_STATE=$GIT_STATE$_GIT_PROMPT_STAGED
   fi
 
