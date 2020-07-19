8088b16ddfd47c0217fae8255cd0e54c862e4bbc
Joao C Vieira <joaocv3@gmail.com>
2020-07-19 10:51:57 -0300

    fix: Export basher root path via HOME environment variable

--- zsh/conf/plugins/basher.zsh
+++ zsh/conf/plugins/basher.zsh
@@ -4,7 +4,7 @@ export PATH="$HOME/.basher/bin:$PATH"
 #eval "$(basher init -)"
 
 export BASHER_SHELL=zsh
-export BASHER_ROOT=/Users/jv/.basher
+export BASHER_ROOT="$HOME/.basher"
 export PATH="$BASHER_ROOT/cellar/bin:$PATH"
 source "$BASHER_ROOT/completions/basher.zsh"
 fpath=("$BASHER_ROOT/cellar/completions/zsh" $fpath)
