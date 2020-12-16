b0ebb4f496b6f3735c85a12963dbd74a72e91f57
João Vortmann <jvortmann@pm.me>
2020-12-16 19:56:10 -0300

    fix: Properly symlink karabiner config

--- karabiner/setup.sh
+++ karabiner/setup.sh
@@ -1,4 +1,4 @@
 echo ">> [Karaniner] Keyboard setup"
 
 echo ">> [Karabiner]  Setting symlink for 'karabiner.json'"
-ln -sfnv ~/.dots/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
+ln -sfnv ~/.dots/karabiner/ ~/.config/karabiner
