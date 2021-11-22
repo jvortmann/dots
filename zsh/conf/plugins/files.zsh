1f30eccf7c8596170c411f6667e062c411364297
João Vortmann <jvortmann@pm.me>
2021-11-22 08:20:08 -0300

    Add `line` function to list a specific line from a file
    ---
    
    Changelog: feature

--- zsh/conf/plugins/files.zsh
+++ zsh/conf/plugins/files.zsh
@@ -3,3 +3,7 @@
 cleanup_episode_filename() {
   mv -v "$1" "$(echo "$1" | sed -E "s/(.*)\.([sS][[:digit:]]{2}[eE][[:digit:]]{2})(.*)?(\.1080|720).*(\..{3})$/\1 - \2 - \3\5/" | tr "." " " | tr -s " " | sed -E "s/(.*) (.{3})$/\1.\2/")"
 }
+
+line () {
+  head -n"$1" "$2" | tail -n1
+}
