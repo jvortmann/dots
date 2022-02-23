f4f88ce205fe2c9b06b34ca867aab5661ddbd357
João Vortmann <57185+jvortmann@users.noreply.github.com>
2022-02-23 13:44:05 -0300

    Remove repeated language name on status when not using .tool-versions
    ---
    
    Changelog: fix

--- zsh/conf/plugins/asdf.zsh
+++ zsh/conf/plugins/asdf.zsh
@@ -5,7 +5,7 @@ source "/usr/local/opt/asdf/asdf.sh"
 version_info() {
   local language_name=$1
   local version_from_tool_versions=$(grep "$language_name" "$PWD/.tool-versions" &> /dev/null | cut -d' ' -f2)
-  local version_from_language_version=$(ls -a "$PWD" | grep ".$language_name-version" | xargs cat)
+  local version_from_language_version=$(ls -a "$PWD" | grep ".$language_name-version" | xargs cat &> /dev/null | cut -d'-' -f2)
 
   if [[ -n $version_from_tool_versions ]]; then
     version_info_tag $language_name $version_from_tool_versions
