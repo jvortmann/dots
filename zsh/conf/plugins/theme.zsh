34759b929580942ccb39e431e271dd07597adcc4
João Vortmann <57185+jvortmann@users.noreply.github.com>
2023-01-06 10:37:03 -0300

    Add helper functions to switch theme from dark to light and back
    ---
    
    Change-type: feature

---
+++ zsh/conf/plugins/theme.zsh
@@ -0,0 +1,46 @@
+swap-theme-to-dark()
+{
+  swap-theme-from-to "light" "dark"
+
+  swap-theme-refresh
+}
+
+swap-theme-to-light()
+{
+  swap-theme-from-to "dark" "light"
+
+  swap-theme-refresh
+}
+
+swap-theme-from-to()
+{
+  gsed -i"" -E "s/^(colors:.*)-$1/\1-$2/" ~/.dots/alacritty/alacritty.yml
+  gsed -i"" -E "s/_$1/_$2/" ~/.dots/tmux/theme.tmux
+  gsed -i"" -E "s/(--color=)$1/\1$2/" ~/.dots/tmux/switch-popup.tmux
+  gsed -i"" -E "s/$1/$2/" ~/.dots/zsh/conf/plugins/bat.zsh
+  gsed -i"" -E "s/(--color=)$1/\1$2/" ~/.dots/zsh/conf/plugins/fzf.zsh
+  if [[ $1 == "light" ]];
+  then
+    gsed -i"" -E "s/(light = )true/\1false/" ~/.dots/git/config
+  else
+    gsed -i"" -E "s/(light = )false/\1true/" ~/.dots/git/config
+  fi
+}
+
+swap-theme-refresh()
+{
+  tmux source ~/.dots/tmux/theme.tmux
+  tmux source ~/.dots/tmux/switch-popup.tmux
+  source ~/.dots/zsh/conf/plugins/bat.zsh
+  source ~/.dots/zsh/conf/plugins/fzf.zsh
+}
+
+swap-theme()
+{
+  if grep -q "^colors:.*-dark" ~/.dots/alacritty/alacritty.yml;
+  then
+    swap-theme-to-light
+  else
+    swap-theme-to-dark
+  fi
+}
