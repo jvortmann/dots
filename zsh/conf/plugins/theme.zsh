d54dd5e3ab2934c5adc599f179356d7d910228bb
João Vortmann <57185+jvortmann@users.noreply.github.com>
2023-01-18 19:37:00 -0300

    Use bath config to swap theme and remove the need to source env var
    ---
    
    Change-type: feature

--- zsh/conf/plugins/theme.zsh
+++ zsh/conf/plugins/theme.zsh
@@ -17,7 +17,7 @@ swap-theme-from-to()
   gsed -i"" -E "s/^(.*themes\/.*)-$1.yml/\1-$2.yml/" ~/.dots/alacritty/alacritty.yml
   gsed -i"" -E "s/$1/$2/" ~/.dots/tmux/theme.tmux
   gsed -i"" -E "s/(--color=)$1/\1$2/" ~/.dots/tmux/switch-popup.tmux
-  gsed -i"" -E "s/$1/$2/" ~/.dots/zsh/conf/plugins/bat.zsh
+  gsed -i"" -E "s/$1/$2/" ~/.config/bat/config
   gsed -i"" -E "s/(--color=)$1/\1$2/" ~/.dots/zsh/conf/plugins/fzf.zsh
   gsed -i"" -E "s/(set background=)$1/\1$2/" ~/.config/nvim/conf/guioptions
   if [[ $1 == "light" ]];
@@ -32,7 +32,6 @@ swap-theme-refresh()
 {
   tmux source ~/.dots/tmux/theme.tmux
   tmux source ~/.dots/tmux/switch-popup.tmux
-  source ~/.dots/zsh/conf/plugins/bat.zsh
   source ~/.dots/zsh/conf/plugins/fzf.zsh
   echo "run 'swap-theme-refresh' on vim's window and reopen Vim or run ':source ~/.config/nvim/conf/guioptions' to refresh"
 }
@@ -50,7 +49,7 @@ swap-theme()
 swap-theme-to-catppuccin()
 {
   gsed -i"" -E "s/^(.*themes\/)(.*)-(.*).yml/\1catppuccin-\3.yml/" ~/.dots/alacritty/alacritty.yml
-  gsed -i"" -E "s/Solarized \((.*)\)/Catppuccin-\1/" ~/.dots/zsh/conf/plugins/bat.zsh
+  gsed -i"" -E "s/Solarized \((.*)\)/Catppuccin-\1/" ~/.config/bat/config
   gsed -i"" -E "s/(colorscheme )Solarized8/\1catppuccin/" ~/.config/nvim/conf/guioptions
 
   swap-theme-refresh
@@ -59,7 +58,7 @@ swap-theme-to-catppuccin()
 swap-theme-to-solarized()
 {
   gsed -i"" -E "s/^(.*themes\/)(.*)-(.*).yml/\1solarized-\3.yml/" ~/.dots/alacritty/alacritty.yml
-  gsed -i"" -E "s/Catppuccin-(.*)\"/Solarized (\1)\"/" ~/.dots/zsh/conf/plugins/bat.zsh
+  gsed -i"" -E "s/Catppuccin-(.*)\"/Solarized (\1)\"/" ~/.config/bat/config
   gsed -i"" -E "s/(colorscheme )catppuccin/\1Solarized8/" ~/.config/nvim/conf/guioptions
 
   swap-theme-refresh
