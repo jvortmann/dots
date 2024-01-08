609dd8ef6903bf6c5531470d058d951e2ad2601b
Joao Vortmann <jvortmann@gmail.com>
2024-01-07 21:33:51 -0300

    Fix missing `~` from path
    ---
    
    Change-type: fix

--- setup-tmux.sh
+++ setup-tmux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Cloning Tmux Plugin Manager to ~/.tmux/plugins"
-mkdir -pv .tmux/plugins/
+mkdir -pv ~/.tmux/plugins/
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 echo "Press 'prefix + I' after starting tmux to install the plugins"
