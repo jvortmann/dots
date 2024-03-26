17525284a0c06bdf6c95181f26d22609892a0493
Joao Vortmann <jvortmann@gmail.com>
2024-03-26 14:14:19 -0300

    Update homebrew shellenv data for Intel
    ---
    
    Change-type: feature

--- zsh/conf/plugins/brew.zsh
+++ zsh/conf/plugins/brew.zsh
@@ -1,7 +1,12 @@
 # vim: ft=zsh
 
 if [[ $(uname -m) = "x86_64" ]]; then
-  export BREW_PREFIX=/usr/local
+  export HOMEBREW_PREFIX="/usr/local";
+  export HOMEBREW_CELLAR="/usr/local/Cellar";
+  export HOMEBREW_REPOSITORY="/usr/local/Homebrew";
+  export PATH="/usr/local/bin:/usr/local/sbin${PATH+:$PATH}";
+  export MANPATH="/usr/local/share/man${MANPATH+:$MANPATH}:";
+  export INFOPATH="/usr/local/share/info:${INFOPATH:-}";
 else
 fi
 export HOMEBREW_NO_AUTO_UPDATE=1
