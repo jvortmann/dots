4cc23f911c0d48a1d441f2ae451013be7d3abe71
Joao Vortmann <jvortmann@gmail.com>
2024-03-26 13:16:27 -0300

    Set brew prefix with `/usr/local` only on Intel
    ---
    
    Change-type: feature

--- zsh/conf/plugins/brew.zsh
+++ zsh/conf/plugins/brew.zsh
@@ -1,7 +1,9 @@
 # vim: ft=zsh
 
-#export BREW_PREFIX=$(brew --prefix)
-export BREW_PREFIX=/usr/local
+if [[ $(uname -m) = "x86_64" ]]; then
+  export BREW_PREFIX=/usr/local
+else
+fi
 export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_NO_INSTALL_CLEANUP=1
 export HOMEBREW_NO_ANALYTICS=1
