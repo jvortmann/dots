9c5c7f89d00037da07b47412ccee6e221bda0188
Joao Vortmann <jvortmann@gmail.com>
2024-03-26 15:08:33 -0300

    Add brew setup for non Intel
    ---
    
    Change-type: feature

--- zsh/conf/plugins/brew.zsh
+++ zsh/conf/plugins/brew.zsh
@@ -8,7 +8,14 @@ if [[ $(uname -m) = "x86_64" ]]; then
   export MANPATH="/usr/local/share/man${MANPATH+:$MANPATH}:";
   export INFOPATH="/usr/local/share/info:${INFOPATH:-}";
 else
+  export HOMEBREW_PREFIX="/opt/homebrew";
+  export HOMEBREW_CELLAR="/opt/homebrew/Cellar";
+  export HOMEBREW_REPOSITORY="/opt/homebrew";
+  export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}";
+  export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:";
+  export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
 fi
+
 export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_NO_INSTALL_CLEANUP=1
 export HOMEBREW_NO_ANALYTICS=1
