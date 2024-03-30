b65e58d97ef586b1381706624c264ba45b8799b4
Joao Vortmann <jvortmann@gmail.com>
2024-03-30 11:12:47 -0300

    Move path config out of brew plugin
    ---
    
    Change-type: feature

--- zsh/conf/plugins/brew.zsh
+++ zsh/conf/plugins/brew.zsh
@@ -4,16 +4,10 @@ if [[ $(uname -m) = "x86_64" ]]; then
   export HOMEBREW_PREFIX="/usr/local";
   export HOMEBREW_CELLAR="/usr/local/Cellar";
   export HOMEBREW_REPOSITORY="/usr/local/Homebrew";
-  export PATH="/usr/local/bin:/usr/local/sbin${PATH+:$PATH}";
-  export MANPATH="/usr/local/share/man${MANPATH+:$MANPATH}:";
-  export INFOPATH="/usr/local/share/info:${INFOPATH:-}";
 else
   export HOMEBREW_PREFIX="/opt/homebrew";
   export HOMEBREW_CELLAR="/opt/homebrew/Cellar";
   export HOMEBREW_REPOSITORY="/opt/homebrew";
-  export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}";
-  export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:";
-  export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
 fi
 
 export HOMEBREW_NO_AUTO_UPDATE=1
