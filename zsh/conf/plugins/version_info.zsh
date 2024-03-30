27398d4252c3ee9fe446b16d5b91edc06f78ead5
Joao Vortmann <jvortmann@gmail.com>
2024-03-30 16:54:16 -0300

    Check language versions using mise
    ---
    
    Change-type: feature

--- zsh/conf/plugins/version_info.zsh
+++ zsh/conf/plugins/version_info.zsh
@@ -22,17 +22,18 @@ language_info() {
 
 version_info() {
   local language_name=$1
+  local version_from_mise=$(mise current $language_name 2>/dev/null)
   local version_from_tool_versions=$(grep "$language_name" "$PWD/.tool-versions" &> /dev/null | cut -d' ' -f2)
   local version_from_language_version=$(ls -a "$PWD" | grep ".$language_name-version" | xargs cat &> /dev/null | cut -d'-' -f2)
 
-  if [[ -n $version_from_tool_versions ]]; then
+  if [[ -n $version_from_mise ]]; then
+    version_info_tag $language_name $version_from_mise
+  elif [[ -n $version_from_tool_versions ]]; then
     version_info_tag $language_name $version_from_tool_versions
+  elif [[ -n $version_from_language_version ]]; then
+    version_info_tag $language_name $version_from_language_version
   else
-    if [[ -n $version_from_language_version ]]; then
-      version_info_tag $language_name $version_from_language_version
-    else
-      echo ""
-    fi
+    echo ""
   fi
 }
 
