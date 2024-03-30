b21ea8192a925adf4519e9b0c94c92090e9a7fa6
Joao Vortmann <jvortmann@gmail.com>
2024-03-30 16:31:05 -0300

    Move language info function into version info
    ---
    
    Change-type: feature

--- zsh/conf/plugins/version_info.zsh
+++ zsh/conf/plugins/version_info.zsh
@@ -1,5 +1,25 @@
 # vim: ft=zsh
 
+language_info() {
+  if declare -f version_info > /dev/null; then
+    local _clojure_info="${ORANGE}$(version_info clojure)${RESET}"
+    local _elixir_info="${PURPLE}$(version_info elixir)${RESET}"
+    local _erlang_info="${MAGENTA}$(version_info erlang)${RESET}"
+    local _go_info="${LIGHT_BLUE}$(version_info golang)${RESET}"
+    local _java_info="${BROWN}$(version_info java)${RESET}"
+    local _js_info="${HI_YELLOW}$(version_info nodejs)${RESET}"
+    local _deno_info="${GREEN}$(version_info deno)${RESET}"
+    local _lua_info="${DEEP_PURPLE}$(version_info lua)${RESET}"
+    local _python_info="${HI_BLUE}$(version_info python)${RESET}"
+    local _ruby_info="${RED}$(version_info ruby)${RESET}"
+    local _rust_info="${LIGHT_BROWN}$(version_info rust)${RESET}"
+
+    echo "${_ruby_info}${_rust_info}${_elixir_info}${_erlang_info}${_java_info}${_clojure_info}${_lua_info}${_js_info}${_deno_info}${_python_info}${_go_info}"
+  else
+    echo ""
+  fi
+}
+
 version_info() {
   local language_name=$1
   local version_from_tool_versions=$(grep "$language_name" "$PWD/.tool-versions" &> /dev/null | cut -d' ' -f2)
--- zsh/themes/jvortmann.zsh
+++ zsh/themes/jvortmann.zsh
@@ -17,31 +17,11 @@ elapsed_info() {
   fi
 }
 
-language_info() {
-  if declare -f version_info > /dev/null; then
-    local _clojure_info="${ORANGE}$(version_info clojure)${RESET}"
-    local _elixir_info="${PURPLE}$(version_info elixir)${RESET}"
-    local _erlang_info="${MAGENTA}$(version_info erlang)${RESET}"
-    local _go_info="${LIGHT_BLUE}$(version_info golang)${RESET}"
-    local _java_info="${BROWN}$(version_info java)${RESET}"
-    local _js_info="${HI_YELLOW}$(version_info nodejs)${RESET}"
-    local _deno_info="${GREEN}$(version_info deno)${RESET}"
-    local _lua_info="${DEEP_PURPLE}$(version_info lua)${RESET}"
-    local _python_info="${HI_BLUE}$(version_info python)${RESET}"
-    local _ruby_info="${RED}$(version_info ruby)${RESET}"
-    local _rust_info="${LIGHT_BROWN}$(version_info rust)${RESET}"
-
-    echo "${_ruby_info}${_rust_info}${_elixir_info}${_erlang_info}${_java_info}${_clojure_info}${_lua_info}${_js_info}${_deno_info}${_python_info}${_go_info}"
-  else
-    echo ""
-  fi
-}
-
 update_prompt() {
   # left prompt
   _user_and_host="${BLUE}%n@%m${RESET}"
   _cwd="${GREEN}%1~${RESET}"
-  
+
   # display exitcode on the right when >0
   _return_code="%(0?..${RED}%?↵${RESET})"
 
