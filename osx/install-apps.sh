5c97604823e5f2688b7b48834f6f5a77aa600943
Joao Vortmann <jvortmann@gmail.com>
2024-04-01 10:05:52 -0300

    Add brew to path before continuing the installation
    ---
    
    Change-type: feature

--- osx/install-apps.sh
+++ osx/install-apps.sh
@@ -4,6 +4,11 @@ xcode-select --install
 sudo xcodebuild -license accept
 
 command -v brew > /dev/null || (echo; echo ">> Homebrew" && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)")
+if [[ $(uname -m) = "x86_64" ]]; then
+  eval "$(/usr/local/bin/brew shellenv)"
+else
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
 
 echo; echo ">> Installing Apps"
 brew bundle --verbose --no-lock
