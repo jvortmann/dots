46573d6cb485757954e3c835ea94360378ad60d7
Joao Vortmann <jvortmann@gmail.com>
2024-04-15 18:51:30 -0300

    Setup colima symlink to docker socket
    ---
    
    Change-type: feature

--- colima/setup.sh
+++ colima/setup.sh
@@ -1,3 +1,4 @@
 echo; echo ">> Setting symlinks for colima"
 mkdir -p ~/.colima/_templates/
 ln -sfnv ~/.dots/colima/default.yaml ~/.colima/_templates/default.yaml
+sudo ln -sfnv ~/.colima/docker.sock /var/run/docker.sock
