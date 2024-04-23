424b521c8ea2f542d4c6c1b545691c93eaed9ac9
Joao Vortmann <jvortmann@gmail.com>
2024-04-23 11:14:47 -0300

    Setup FZF keybindings
    ---
    
    Change-type: feature

--- zsh/conf/plugins/fzf.zsh
+++ zsh/conf/plugins/fzf.zsh
@@ -1,2 +1,4 @@
 export FZF_DEFAULT_COMMAND="rg --files -. -g '!**/vendor/cache/**/*.gem' -g '!.git/**'"
 export FZF_DEFAULT_OPTS="--color=dark --border=sharp --preview-window=border-sharp"
+
+eval "$(fzf --zsh)"
