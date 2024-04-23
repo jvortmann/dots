e7387ab3ebf5dc446bdb8e493b33e5f34a187293
Joao Vortmann <jvortmann@gmail.com>
2024-04-23 16:19:11 -0300

    Use FD as default FZF command and add extra ones
    ---
    
    Change-type: feature

--- zsh/conf/plugins/fzf.zsh
+++ zsh/conf/plugins/fzf.zsh
@@ -1,4 +1,24 @@
-export FZF_DEFAULT_COMMAND="rg --files -. -g '!**/vendor/cache/**/*.gem' -g '!.git/**'"
 export FZF_DEFAULT_OPTS="--color=dark --border=sharp --preview-window=border-sharp"
+export FZF_DEFAULT_COMMAND="fd  --type file --type symlink --hidden --strip-cwd-prefix --exclude .git --exclude '*.keep'"
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND="fd --type directory --hidden --strip-cwd-prefix --exclude .git"
 
 eval "$(fzf --zsh)"
+
+export FZF_CTRL_T_OPTS="--preview 'bat -n --color=always --line-range :500 {}'"
+export FZF_ALT_C_OPTS="--preview 'lsd --tree --color=always {} | head -200'"
+
+# Advanced customization of fzf options via _fzf_comprun function
+# - The first argument to the function is the name of the command.
+# - You should make sure to pass the rest of the arguments to fzf.
+_fzf_comprun() {
+  local command=$1
+  shift
+
+  case "$command" in
+    cd)           fzf --preview 'lsd --tree --color=always {} | head -200' "$@" ;;
+    export|unset) fzf --preview "eval 'echo $'{}"         "$@" ;;
+    ssh)          fzf --preview 'dig {}'                   "$@" ;;
+    *)            fzf --preview "bat -n --color=always --line-range :500 {}" "$@" ;;
+  esac
+}
