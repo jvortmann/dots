8dd9768e395bb3f3d63ea5d6e966bd0522fea60a
Joao Vortmann <jvortmann@gmail.com>
2024-10-01 13:32:59 -0300

    Update alias to update blame ignore file for style commits
    ---
    
    Change-type: feature

--- zsh/conf/plugins/git-ignore-styles.zsh
+++ zsh/conf/plugins/git-ignore-styles.zsh
@@ -1,4 +1,4 @@
-# Show Git branch/tag, or name-rev if on detached head
+# Update .git-blame-ignore-revs file with commits from the branch that have the `Change-type: style` trailer
 git-ignore-styles() {
-  cat <(git rev-list $1 --grep='Change-type: style' --no-commit-header --format='%H # %s' --reverse) <(cat .git-blame-ignore-revs) | sort | uniq > .git-blame-ignore-revs
+  cat <(git rev-list $1 --grep='Change-type: style' --no-commit-header --format='%H # %s' --reverse) .git-blame-ignore-revs | sort | uniq > .git-blame-ignore-revs.temp && mv .git-blame-ignore-revs{.temp,}
 }
